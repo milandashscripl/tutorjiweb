1024e0f38cead4ec7650ba226b0d9dcf6fd5bbe3
milandashscripl <immilankumar01@gmail.com>
2025-03-21 09:54:30 +0530

    desgin

--- js/script.js
+++ js/script.js
@@ -151,3 +151,20 @@ document.getElementById("queryForm").addEventListener("submit", async (event) =>
   }
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const bubbleContainer = document.querySelector(".hero");
+  for (let i = 0; i < 10; i++) {
+    let bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+    let size = Math.random() * 40 + 10;
+    let position = Math.random() * 100;
+    let duration = Math.random() * 5 + 5;
+    bubble.style.width = `${size}px`;
+    bubble.style.height = `${size}px`;
+    bubble.style.left = `${position}%`;
+    bubble.style.animationDuration = `${duration}s`;
+    bubbleContainer.appendChild(bubble);
+  }
+});
